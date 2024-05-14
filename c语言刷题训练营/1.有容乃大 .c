7c8f66bda38a2d30f144c54cb11eec19488dbf84
John Green <18172745446@163.com>
2024-05-14 11:21:54 +0800

    Create 1.有容乃大 .c

---
+++ c语言刷题训练营/1.有容乃大 .c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+   pritnf("The size of short is %d bytes",sizeof(short));
+   pritnf("The size of int is %d bytes",sizeof(int));
+   pritnf("The size of long is %d bytes",sizeof(long));
+   pritnf("The size of long long is %d bytes",sizeof(long long));
+   return 0;
+}
