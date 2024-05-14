11435c9a98ba2487b670e92da2c9687a6ce99cd8
John Green <18172745446@163.com>
2024-05-14 11:48:10 +0800

    Update 1.有容乃大 .c

--- c语言刷题训练营/1.有容乃大 .c
+++ c语言刷题训练营/1.有容乃大 .c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
 int main() {
-   pritnf("The size of short is %d bytes",sizeof(short));
-   pritnf("The size of int is %d bytes",sizeof(int));
-   pritnf("The size of long is %d bytes",sizeof(long));
-   pritnf("The size of long long is %d bytes",sizeof(long long));
+   printf("The size of short is %d bytes",sizeof(short));
+   printf("The size of int is %d bytes",sizeof(int));
+   printf("The size of long is %d bytes",sizeof(long));
+   printf("The size of long long is %d bytes",sizeof(long long));
    return 0;
 }
